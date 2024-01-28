1876740a37b4fae98c35cf9c1e3889fed2dec516
jacpa2011 <156595952+jacpa2011@users.noreply.github.com>
2024-01-28 15:49:40 +0100

    cooldown and label off of the bottle gone

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const WaterSell = document.querySelector("#sellwater")
 const WaterReturn = document.querySelector("#returnwater")
 const WaterBottle = document.getElementById("waterbottle")
 const WaterDrink = document.querySelector("#drinkwater")
+const WaterDrinkCooldown = document.querySelector("#drinkwatercooldown")
 
 let player = {
     money: new Decimal(0.5),
@@ -15,7 +16,9 @@ let player = {
         sell: new Decimal(0.4),
         waterbottleamount: new Decimal(0),
         waterbottleamountmax: new Decimal(205),
-        drinkamount: new Decimal(41)
+        drinkamount: new Decimal(41),
+        drinkcooldown: new Decimal(0),
+        drinkcooldownmax: new Decimal(1.5),
     }
 }
 
@@ -23,8 +26,15 @@ setInterval(function() {
     MoneyLabel.textContent = `\u20ac${parseFloat(format(player.money)).toFixed(2)}`
     WaterAmount.textContent = `You have  ${player.water.amount} full water bottle(s)`
     WaterEmptyAmount.textContent = `You have ${player.water.emptyamount} empty water bottle(s)`
+    WaterDrinkCooldown.textContent = `Cooldown: ${parseFloat(format(player.water.drinkcooldown)).toFixed(2)}s`
     WaterReturn.textContent = `Returns: \u20ac${parseFloat(format(player.water.sell)).toFixed(2)}`
     WaterBottle.style.height = parseFloat(format(Decimal.div(player.water.waterbottleamount, player.water.waterbottleamountmax).mul(205)))
+    if (player.water.drinkcooldown.gte(0.01)) {
+        player.water.drinkcooldown = player.water.drinkcooldown.sub(new Decimal(1).div(50))
+    }
+    else {
+        player.water.drinkcooldown = new Decimal(0)
+    }
 }, 20)
 
 WaterSell.addEventListener("click", function() {
@@ -43,14 +53,17 @@ WaterBuy.addEventListener("click", function() {
 })
 
 WaterDrink.addEventListener("click", function() {
-    if (player.water.amount.gte(0.1)) {
-        player.water.waterbottleamount = player.water.waterbottleamount.sub(player.water.drinkamount)
-        if (player.water.waterbottleamount.lte(0)) {
-            player.water.waterbottleamount = new Decimal(0)
-            player.water.amount = player.water.amount.sub(1)
-            player.water.emptyamount = player.water.emptyamount.add(1)
-            if (player.water.amount.gte(new Decimal(0.1))) {
-                player.water.waterbottleamount = player.water.waterbottleamountmax
+    if (player.water.drinkcooldown.lte(0.03)) {
+        if (player.water.amount.gte(0.1)) {
+            player.water.drinkcooldown = player.water.drinkcooldownmax
+            player.water.waterbottleamount = player.water.waterbottleamount.sub(player.water.drinkamount)
+            if (player.water.waterbottleamount.lte(0)) {
+                player.water.waterbottleamount = new Decimal(0)
+                player.water.amount = player.water.amount.sub(1)
+                player.water.emptyamount = player.water.emptyamount.add(1)
+                    if (player.water.amount.gte(new Decimal(0.1))) {
+                        player.water.waterbottleamount = player.water.waterbottleamountmax
+                    }
             }
         }
     }
